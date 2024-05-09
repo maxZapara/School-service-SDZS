673e98b96005d3e078cc2d263996cee459f3bd17
SkwishStar <sviatpol11@example.com>
2024-05-09 20:09:39 +0200

    Create user model

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,19 @@ from flask import Flask
 
 
 def create_app():
-    from core import main_blp
     from .extensions import db
     app = Flask(__name__, static_url_path='/static')
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
 
+    from core import main_blp
     app.register_blueprint(main_blp)
+
+    from users import user_blp
+    app.register_blueprint(user_blp)
+    
+    with app.app_context():
+        db.create_all()
 
     return app
