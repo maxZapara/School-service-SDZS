2c11922b4c589e6357a5982cd697fe57a6dcbc4c
Eloy527 <davidserba302@gmail.com>
2024-05-09 21:30:20 +0300

    changed the file tree and edited the registration

--- core/routes.py
+++ core/routes.py
@@ -2,7 +2,7 @@ from . import main_blp
 from flask import render_template
 
 @main_blp.route("/")
-def hello_world():
+def home():
     return render_template('homepage/homepage.html')
 
 @main_blp.route("/login")
